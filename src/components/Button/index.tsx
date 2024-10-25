ff27ab4bdb5aa3c56a1c4c6afec2e79d8543038b
Lucas Siqueira <wefit.siqueira@voeazul.com.br>
2024-10-25 17:24:41 -0300

    feat: nova funcionalidade

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ButtonComponente: React.FC = () => {
-  return <button type="submit">Clique </button>;
+  return <button type="submit">Clique no vertto 🥸</button>;
 };
 
 export default ButtonComponente;
